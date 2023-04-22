d440c7fb38af2c1b50e8989079ae74d3854af414
folabia1 <27552121+folabia1@users.noreply.github.com>
2023-04-22 18:36:24 +0100

    create title bar component

---
+++ vue-app/src/components/TitleBar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="title-bar">
+    <div class="menu-button">Menu</div>
+    <h1 class="title">Collab Club</h1>
+    <div class="spacer" />
+    <div class="shadow" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title-bar {
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  align-items: center;
+
+  .menu-button,
+  .spacer {
+    flex: 0 0 3.5rem;
+  }
+
+  .menu-button {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .title {
+    flex-grow: 1;
+    text-align: center;
+  }
+
+  .shadow {
+    position: absolute;
+    width: 100%;
+    height: 10px;
+    bottom: -10px;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0.2), transparent);
+  }
+}
+</style>
