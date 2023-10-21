46e5645370f8e72764bb1a594da851ae4b50f0b8
folabia1 <27552121+folabia1@users.noreply.github.com>
2023-10-21 17:53:04 +0100

    remove unused function

--- react-app/src/logic/api.js
+++ react-app/src/logic/api.js
@@ -6,20 +6,3 @@ export const getAvailableGenreSeeds = httpsCallable(functions, "Spotify-getAvail
 export const getRandomStartingArtists = httpsCallable(functions, "Spotify-getRandomStartingArtists");
 export const getArtistWithPhotoUrl = httpsCallable(functions, "Spotify-getArtistWithPhotoUrl");
 export const searchForTracksWithQuery = httpsCallable(functions, "Spotify-searchForTracksWithQuery");
-
-export async function suggestTracks(trackGuess, filter) {
-  try {
-    const tracksResponse = await searchForTracksWithQuery({
-      trackName: trackGuess,
-      artistName: this.currentPathArtist?.name ?? "",
-      requireMultipleArtists: filter,
-      requireThisArtist: filter,
-      requireSimilarName: true,
-      strictMode: false,
-    });
-    // display tracks for user to choose from
-    this.setSuggestedTracks(tracksResponse.data);
-  } finally {
-    this.setIsLoadingResults(false);
-  }
-}
