3bba5ea6a44e1601b555201281a589aca3af70cb
folabia1 <27552121+folabia1@users.noreply.github.com>
2023-10-22 15:41:15 +0100

    dark mode by default

--- react-app/src/components/GenreChip.jsx
+++ react-app/src/components/GenreChip.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useColorTheme } from "../hooks/useColorTheme";
+
 const StyledGenreChip = styled.button`
   border-radius: 18px;
   padding: 0.2rem 1.2rem;
@@ -10,26 +12,19 @@ const StyledGenreChip = styled.button`
   font-size: 1.8rem;
   font-weight: 700;
   text-align: center;
-
-  @media (prefers-color-scheme: dark) {
-    color: var(--accent);
-  }
+  color: ${(props) => (props.colorTheme === "dark-mode" ? "var(--accent)" : "var(--text-color)")};
 
   &:disabled {
     border-color: var(--disabled);
     background-color: transparent;
     cursor: not-allowed;
-    @media (prefers-color-scheme: dark) {
-      color: var(--disabled);
-    }
+    color: ${(props) => (props.colorTheme === "dark-mode" ? "var(--disabled)" : "var(--text-color)")};
   }
 
   &.active {
     border-color: var(--accent);
     background-color: var(--accent);
-    @media (prefers-color-scheme: dark) {
-      color: var(--background-primary);
-    }
+    color: ${(props) => (props.colorTheme === "dark-mode" ? "var(--background-primary)" : "var(--text-color)")};
   }
 
   &:disabled.active {
@@ -38,9 +33,15 @@ const StyledGenreChip = styled.button`
   }
 `;
 
-export default function GenreChip({ text, active, onClick, disabled }) {
+export default function GenreChip({ text, active, onClick = () => {}, disabled }) {
+  const [colorTheme] = useColorTheme();
   return (
-    <StyledGenreChip className={`genre-chip${active ? " active" : ""}`} onClick={() => onClick(text)} disabled={disabled}>
+    <StyledGenreChip
+      colorTheme={colorTheme}
+      className={`genre-chip${active ? " active" : ""}`}
+      onClick={() => onClick(text)}
+      // disabled={disabled}
+    >
       {text}
     </StyledGenreChip>
   );
